a9403b76299b33f1280f126e0c4bd070ad3dc924
Algopaul <72927083+Algopaul@users.noreply.github.com>
2024-04-17 17:08:29 -0400

    add dnn train code

---
+++ fireuq/dnn/dnn_optimize.py
@@ -0,0 +1,113 @@
+import optax
+from functools import partial
+import jax
+from absl import flags, logging
+import numpy as np
+
+_SCHEDULE_LEARNING_RATE = flags.DEFINE_bool(
+    'schedule_learning_rate', True, 'Whether to use a learning_rate scheduler.')
+_START_LEARNING_RATE = flags.DEFINE_float(
+    'start_learning_rate', 1e-3, 'The base/initial learning rate')
+_END_LEARNING_RATE = flags.DEFINE_float(
+    'end_learning_rate', 1e-8, 'The final learning rate')
+_N_WARMUP_STEPS = flags.DEFINE_integer(
+    'n_warmup_steps', 50, 'The number of warmup steps')
+_N_OPTIMIZATION_STEPS = flags.DEFINE_integer(
+    'n_optimization_steps', 100000, 'The number of optimization steps')
+_INITIALIZATION_LOSS_TOL = flags.DEFINE_float(
+    'initialization_loss_tol',
+    0.0,
+    'Breaks the optimization loop if loss is below this value.')
+
+
+TRAIN_LOSSES = []
+VAL_LOSSES = []
+ITERS = []
+
+OPTIMIZER_DICT = {
+    "adam": optax.adam,
+    "adamw": optax.adamw,
+    "adamww": partial(optax.adamw, weight_decay=1e-3),
+    "amsgrad": optax.amsgrad,
+    "adabelief": optax.adabelief,
+}
+
+
+def get_optimizer(optimizer_name, learning_rate) -> optax.GradientTransformation:
+  opti_f = OPTIMIZER_DICT[optimizer_name]
+  return opti_f(learning_rate=learning_rate)
+
+
+def standard_loss(param, x_train, dnn, y_train):
+  vmapped_dnn=jax.vmap(dnn.apply, in_axes=[None, 0])
+  return jax.numpy.mean(
+      jax.numpy.squeeze(vmapped_dnn(param, x_train)-y_train)**2
+      )
+
+
+def fit_dnn(
+    param_init,
+    args,
+    loss,
+    val_data,
+    steps=None,
+    learning_rate=None,
+    loss_tol=None,
+    optimizer_name="adam",
+):
+  if loss_tol is None:
+    loss_tol = _INITIALIZATION_LOSS_TOL.value
+  if learning_rate is None:
+    learning_rate = _START_LEARNING_RATE.value
+  if steps is None:
+    steps = _N_OPTIMIZATION_STEPS.value
+  if _SCHEDULE_LEARNING_RATE.value:
+    learning_rate = optax.warmup_cosine_decay_schedule(
+        init_value=0.0,
+        warmup_steps=_N_WARMUP_STEPS.value,
+        peak_value=_START_LEARNING_RATE.value,
+        decay_steps=_N_OPTIMIZATION_STEPS.value,
+        end_value=_END_LEARNING_RATE.value
+    )
+  optimizer = get_optimizer(optimizer_name, learning_rate)
+  opt_state = optimizer.init(param_init)
+
+  @jax.jit
+  def step(params, opt_state, args):
+    loss_value, grads = jax.value_and_grad(loss)(params, *args)
+    updates, opt_state = optimizer.update(grads, opt_state, params)
+    params = optax.apply_updates(params, updates)
+    return params, opt_state, loss_value
+
+  loss_value = 0.0
+
+  params = param_init
+  final_params = param_init
+  val_loss = loss(params, *val_data)
+
+  print('steps')
+  print(steps)
+  print('steps')
+  for i in range(steps):
+    params, opt_state, loss_value = step(params, opt_state, args)
+    if i % 100 == 0:
+      ITERS.append(i)
+      TRAIN_LOSSES.append(loss_value)
+      c_val_loss = loss(params, *val_data)
+      VAL_LOSSES.append(c_val_loss)
+      if c_val_loss < val_loss:
+        final_params = params
+        val_loss = c_val_loss
+      logging.info(f'step {i}, loss: {loss_value:.5e}')
+    if loss_value < loss_tol:
+      break
+
+  return final_params, loss_value
+
+def collect_train_log():
+  iters = np.stack(ITERS)
+  train_losses = np.stack(TRAIN_LOSSES)
+  val_losses = np.stack(VAL_LOSSES)
+  A = np.stack((iters, train_losses, val_losses))
+  return A
+
---
+++ fireuq/dnn/train_dnn.py
@@ -0,0 +1,84 @@
+import jax
+import numpy as np
+import jax.numpy as jnp
+import flax.linen as nn
+from flax.serialization import to_bytes
+from absl import flags, app
+import fireuq.dnn.dnn_optimize as dnopt
+
+DNN_LAYERS = flags.DEFINE_multi_integer('dnn_layers', [5,5,5], 'The layers of the DNN')
+KEY_SEED = flags.DEFINE_integer('key_seed', 0, 'The DNN seed key')
+TRAIN_DATA_FILE = flags.DEFINE_string('train_data', '', 'The file containing the train data')
+TRAIN_TEST_SPLIT_IDX = flags.DEFINE_integer('train_test_split_idx', 900, 'Where to split data into train and validation')
+OUTNAME = flags.DEFINE_string('outname', 'desc', 'The output data name.')
+OUTDIR = flags.DEFINE_string('outdir', '', 'The output directory')
+
+class BaseMLP(nn.Module):
+
+  @nn.compact
+  def __call__(self, x) -> jax.Array:
+    layers=[3, *DNN_LAYERS.value, 1]
+    for layer in layers:
+      x = nn.Dense(layer)(x)
+      x = nn.selu(x)
+    return x
+
+
+def load_data(datafile=None):
+  if datafile is None:
+    datafile=TRAIN_DATA_FILE.value
+  data = np.loadtxt(datafile, skiprows=1)  
+  x = data[:, :3]
+  y = data[:, 3]
+  return x, y
+
+
+def normalize_data(x, mean_val=None, std_val=None):
+  if mean_val is None:
+    mean_val = np.mean(x, axis=0)
+  if std_val is None:
+    std_val = np.std(x, axis=0)
+  return (x-mean_val)/std_val, mean_val, std_val
+
+def denormalize_data(x, mean_val, std_val):
+  return std_val*x+mean_val
+
+
+def get_dnn():
+  dnn=BaseMLP()
+  sample = jnp.ones(3)
+  params=dnn.init(jax.random.key(KEY_SEED.value), sample)
+  return dnn, params
+
+
+def main(_):
+  x, y = load_data() 
+  x, x_mean, x_std = normalize_data(x)
+  y, y_mean, y_std = normalize_data(y)
+  dnn, init_params = get_dnn()
+  tt_split = TRAIN_TEST_SPLIT_IDX.value
+  x_train, x_test = x[:tt_split], x[tt_split:]
+  y_train, y_test = y[:tt_split], y[tt_split:]
+
+  @jax.jit
+  def mse(params, x_batched, y_batched):
+    # Define the squared loss for a single pair (x,y)
+    def squared_error(x, y):
+      pred = dnn.apply(params, x)
+      return jnp.inner(y-pred, y-pred) / 2.0
+    # Vectorize the previous to compute the average of the loss on all samples.
+    return jnp.mean(jax.vmap(squared_error)(x_batched,y_batched), axis=0)
+
+  mse(init_params, x_train, y_train)
+  
+  final_params, _ = dnopt.fit_dnn(init_params, (x_train, y_train), mse, (x_test, y_test))
+  A = dnopt.collect_train_log()
+  np.savetxt(OUTDIR.value+f'loss_log_{OUTNAME.value}.txt', A.T, fmt='%.4e')
+  bb = to_bytes(final_params)
+  with open(OUTDIR.value+'dnn_bytes_'+OUTNAME.value, 'wb') as file:
+    file.write(bb)
+  np.savetxt(OUTDIR.value+'dnn_means_'+OUTNAME.value, np.array([*x_mean, *x_std, y_mean, y_std]))
+  pass
+
+if __name__ == "__main__":
+  app.run(main)
